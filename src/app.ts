d9cd420884bcfa9fc49ecc6556808ad00d66580d
Milton Luiz Alvarenga Junior <milton.luiz@infracommerce.com.br>
2022-06-28 02:45:09 -0300

    Create swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
 
 import { decompositionRoutes } from "./routes/decomposition.routes";
 import handler from "./shared/errors/handler";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/decomposition", decompositionRoutes);
 
